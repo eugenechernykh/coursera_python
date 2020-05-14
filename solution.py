0551ca54131832c4394fcd7a5a6b23f52d328886
eugenechernykh <echernykh@gmail.com>
2020-05-15 02:28:26 +0700

    ok

--- solution.py
+++ solution.py
@@ -1,21 +1,12 @@
+def factorial(n):
+    res = 1
+    for i in range(1, n + 1):
+        res *= i
+    return res
+
+
 n = int(input())
-m = int(input())
 
-
-def ReduceFraction(n, m):
-    if n == m:
-        return 1, 1
-    if n > m:
-        for i in range(2, m + 1):
-            if n % i == 0 and m % i == 0:
-                n, m = ReduceFraction(n // i, m // i)
-                return n, m
-    if n < m:
-        for i in range(2, n + 1):
-            if n % i == 0 and m % i == 0:
-                n, m = ReduceFraction(n // i, m // i)
-                return n, m
-    return n, m
-
-
-print(*ReduceFraction(n, m))
+for i in range(1, n):
+    sum += factorial(i)
+print(sum)
